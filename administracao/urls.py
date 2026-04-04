1a57c6a04029a46221e814fe206bfc77742cb891
Nicolas Oliveira <nicolasoliveira1902@gmail.com>
2026-04-04 11:22:05 -0300

    crud de usuario feita

--- administracao/urls.py
+++ administracao/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('servicos/editar/<int:id>', servico_views.editar_servico, name='editar_servico'),
     path('servicos/excluir/<int:id>', servico_views.excluir_servico, name='excluir_servico'),
     path('usuarios/cadastrar/', usuario_views.cadastrar_usuario, name='cadastrar_usuario'),
+    path('usuarios/listar/', usuario_views.listar_usuarios, name='listar_usuarios'),
+    path('usuarios/editar/<int:id>', usuario_views.editar_usuario, name='editar_usuario'),
+    path('usuarios/excluir/<int:id>', usuario_views.excluir_usuario, name='excluir_usuario'),
 ]
