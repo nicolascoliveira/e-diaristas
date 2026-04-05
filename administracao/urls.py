729c57869fffc844618ec4a765c1106b748bf5bd
Nicolas Oliveira <nicolasoliveira1902@gmail.com>
2026-04-05 09:16:26 -0300

    alterando campos do cadastro de usuarios

--- administracao/urls.py
+++ administracao/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from .views import servico_views, usuario_views
 
 urlpatterns = [
+    # Servicos
     path('servicos/cadastrar', servico_views.cadastrar_servico, name='cadastrar_servico'),
     path('servicos/listar', servico_views.listar_servicos, name='listar_servicos'),
     path('servicos/editar/<int:id>', servico_views.editar_servico, name='editar_servico'),
     path('servicos/excluir/<int:id>', servico_views.excluir_servico, name='excluir_servico'),
     path('usuarios/cadastrar/', usuario_views.cadastrar_usuario, name='cadastrar_usuario'),
+    
+    # Usuarios
     path('usuarios/listar/', usuario_views.listar_usuarios, name='listar_usuarios'),
     path('usuarios/editar/<int:id>', usuario_views.editar_usuario, name='editar_usuario'),
     path('usuarios/excluir/<int:id>', usuario_views.excluir_usuario, name='excluir_usuario'),
